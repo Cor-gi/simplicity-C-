ba240356cefcc68236276a1e829be5d38e875566
-Corgi- <corgiplaysvr@gmail.com>
2023-02-06 19:25:15 -0700

    1.1.0

--- package/lib.js
+++ package/lib.js
@@ -2,4 +2,9 @@ const log = (message) => {
     console.log(message)
 }
 
+const error = (message) => {
+    console.error(message)
+}
+
 module.exports = log
+module.exports = error
--- testing/app.js
+++ testing/app.js
@@ -1,3 +1,5 @@
 const log = require('simplicity-c')
+const error = require('simplicity-c')
 
 log('Hello World!')
+error('This is an error!')
